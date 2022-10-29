14edb6122df2bb480cc1870f1e9414c8b959b97b
Ridgal <planetstoun@mail.ru>
2022-10-29 23:30:15 +0300

    first commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.scss';
+
+const Footer:React.FC = () => {
+  return ( 
+    <footer className="info">
+      <p>Created by Ridgal</p>
+      <p>Source on <a href="https://github.com/Ridgal/TodoList">Github</a></p>
+    </footer>
+  )
+};
+
+export { Footer };
